7a6c8f5a019cf8152c271fea9df6b7b48201e58b
Paola Jocelin Septimo Velazquez <Paola Jocelin Septimo Velazquez>
2017-04-06 17:10:36 -0500

    snake solo las bases

---
+++ snake.js
@@ -0,0 +1,12 @@
+var contenedor=document.getElementById("contenedor");
+var cabezaSerpiente=document.getElementById("cabezaSerpiente");
+var posicionX=250;
+var posicionY=250;
+var avanzarX=20;
+var avanzarY=20;
+var teclas={
+  left:37;
+  right:39;
+  up:38;
+  down:40;
+}
